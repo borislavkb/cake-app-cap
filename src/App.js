6cfc2cf52ac4a5a63e9959c08395e45e23bd7df3
Borislav Borislavov <borislavkb@gmail.com>
2021-08-12 23:03:03 +0200

    Add basic html layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <header className="App-header">My cakes</header>
+      <main className="App-main">Content</main>
+      <footer className="App-footer">
+        <ul>
+          <li>MyCakes</li>
+          <li>Plus</li>
+          <li>Favs</li>
+        </ul>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
