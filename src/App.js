69e4e3c0f4a10249ec2e96de0e48cdb6901dcc2a
Borislav Borislavov <borislavkb@gmail.com>
2021-08-12 23:18:50 +0200

    Add Item-cards

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">My cakes</header>
-      <main className="App-main"></main>
+      <main className="App-main">
+        <ul>
+          <li className="Item-card">
+            <img src="" alt="" />
+            <h3 className="Item-card__name">Chocolate cake</h3>
+          </li>
+          <li className="Item-card">
+            <img src="" alt="" />
+            <h3 className="Item-card__name">Chocolate cake</h3>
+          </li>
+          <li className="Item-card">
+            <img src="" alt="" />
+            <h3 className="Item-card__name">Chocolate cake</h3>
+          </li>
+        </ul>
+      </main>
       <footer className="App-footer">
-        <li className="Item-card">
-          <img src="" alt="" />
-          <h3 className="Item-card__name">Chocolate cake</h3>
-        </li>
         <ul>
           <li>MyCakes</li>
           <li>Plus</li>
