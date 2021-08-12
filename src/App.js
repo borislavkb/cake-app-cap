be386fdafa886dac352cff53626e228453331a9b
Borislav Borislavov <borislavkb@gmail.com>
2021-08-12 23:16:24 +0200

    Add basic html Item-card

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">My cakes</header>
-      <main className="App-main">Content</main>
+      <main className="App-main"></main>
       <footer className="App-footer">
+        <li className="Item-card">
+          <img src="" alt="" />
+          <h3 className="Item-card__name">Chocolate cake</h3>
+        </li>
         <ul>
           <li>MyCakes</li>
           <li>Plus</li>
