da6b0633b00e5e4f1ee7d8c9e3463f0dc8f1dbb8
Borislav Borislavov <borislavkb@gmail.com>
2021-08-12 23:27:45 +0200

    Create ItemCard component

---
+++ src/components/ItemCard.js
@@ -0,0 +1,8 @@
+export default function ItemCard() {
+  return (
+    <li className="Item-card">
+      <img src="" alt="" />
+      <h3 className="Item-card__name">Chocolate cake</h3>
+    </li>
+  );
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,13 @@
+import ItemCard from "./ItemCard";
+
 export default function Main() {
   return (
     <main className="App-main">
       <ul>
-        <li className="Item-card">
-          <img src="" alt="" />
-          <h3 className="Item-card__name">Chocolate cake</h3>
-        </li>
-        <li className="Item-card">
-          <img src="" alt="" />
-          <h3 className="Item-card__name">Chocolate cake</h3>
-        </li>
-        <li className="Item-card">
-          <img src="" alt="" />
-          <h3 className="Item-card__name">Chocolate cake</h3>
-        </li>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
       </ul>
     </main>
   );
