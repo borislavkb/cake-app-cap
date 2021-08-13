a759cac5012a9fc81100031cb3817f2117617c27
Borislav Borislavov <borislavkb@gmail.com>
2021-08-13 11:25:49 +0200

    Add color to Header and ItemCard

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,7 +2,7 @@ import "./ItemCard.css";
 
 export default function ItemCard() {
   return (
-    <div>
+    <div className="Item-card frame">
       <li className="Item-card">
         <img src="image" alt="description" />
         <h3 className="Item-card__name">Chocolate cake</h3>
