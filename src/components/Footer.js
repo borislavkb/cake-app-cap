c6e0117b913370256b61b776e69a9aa1e1e01296
Borislav Borislavov <borislavkb@gmail.com>
2021-08-13 10:43:57 +0200

    Add style on Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer className="App-footer">
-      <ul>
+      <ul className="App-footer__navigation">
         <li>MyCakes</li>
         <li>Plus</li>
         <li>Favs</li>
