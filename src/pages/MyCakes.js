19dec5b0491dbf090d3f4d3c5f14a58ad6fa072f
Borislav Borislavov <borislavkb@gmail.com>
2021-08-16 12:49:09 +0200

    Edit MyCakes component

--- src/pages/MyCakes.js
+++ src/pages/MyCakes.js
@@ -2,9 +2,5 @@ import Main from "../components/Main";
 import "./MyCakes.css";
 
 export default function MyCakes() {
-  return (
-    <div className="My-cakes-page">
-      <Main />
-    </div>
-  );
+  return <Main />;
 }
