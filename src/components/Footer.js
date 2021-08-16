3d52f5570da3f98f68745e958455fa56c065c214
Borislav Borislavov <borislavkb@gmail.com>
2021-08-16 16:06:35 +0200

    Import Icon BsPlus<square from react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import "./Footer.css";
 import { NavLink } from "react-router-dom";
+import { BsPlusSquare } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <footer className="Footer">
       <nav className="Footer__navigation">
         <NavLink to="/">MyCakes</NavLink>
-        <NavLink to="/add">Plus</NavLink>
+        <NavLink to="/add">
+          <BsPlusSquare size="2rem" />
+        </NavLink>
         <NavLink to="/favs">Favs</NavLink>
       </nav>
     </footer>
