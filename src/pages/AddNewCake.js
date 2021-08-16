c643fb063b3402ee4851646970c5b42700565003
Borislav Borislavov <borislavkb@gmail.com>
2021-08-16 15:54:51 +0200

    Add input form

---
+++ src/pages/AddNewCake.js
@@ -0,0 +1,31 @@
+import "./AddNewCake.css";
+
+export default function AddNewCake() {
+  return (
+    <form className="form">
+      <input
+        type="file"
+        id="imageInput"
+        name="imageInput"
+        accept="image/png, image/jpeg"
+      />
+      <input type="text" id="name" name="itemName" placeholder="cake name" />
+      <textarea
+        id="inputIngredients"
+        name="inputIngredients"
+        rows="5"
+        cols="40"
+        placeholder="Insert a list of ingredients"
+      ></textarea>
+      <textarea
+        id="inputRecipe"
+        name="inputRecipe"
+        rows="5"
+        cols="40"
+        placeholder="Insert a recipe"
+      ></textarea>
+
+      <input type="submit" value="Submit" className="btn-submit" />
+    </form>
+  );
+}
