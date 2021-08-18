d1d1108cbd1a5b3d9aec965999df1f393a4ea09f
Borislav Borislavov <borislavkb@gmail.com>
2021-08-18 17:08:22 +0200

    Save all

--- src/pages/AddNewCake.js
+++ src/pages/AddNewCake.js
@@ -4,14 +4,33 @@ import InputIngredients from "../components/inputComponents/inputIngredients";
 import ImageInput from "../components/inputComponents/imageInput";
 
 import InputItemName from "../components/inputComponents/InputItemName";
+import { useState } from "react";
 
 export default function AddNewCake() {
+  const [recipes, setRecipes] = useState([]);
+
+  function handleSubmit(event) {
+    event.PreventDefault();
+    const form = event.target;
+    console.log(form);
+    const newRecipe = form.inputRecipe.value;
+
+    const addRecipe = [
+      ...recipes,
+      {
+        id: 33,
+        recipe: newRecipe,
+      },
+    ];
+    setRecipes(addRecipe);
+    form.reset();
+  }
+
   return (
-    <form className="AddNewCake__form">
+    <form className="AddNewCake__form" onSubmit={handleSubmit}>
       <i className="input-file--icon"></i>
       <ImageInput />
       <InputItemName />
-
       <InputIngredients />
       <InputRecipe />
 
