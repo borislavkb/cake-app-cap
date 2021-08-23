5918235d69e300f1f0eead0a76b3d710306a0e53
Borislav Borislavov <borislavkb@gmail.com>
2021-08-23 14:58:29 +0200

    Add Link to ItemCard on Main.js; Add text-decoaration: none to remove the links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,20 @@
 import "./Main.css";
 import ItemCard from "./ItemCard";
+import { Link } from "react-router-dom";
+import { directive } from "@babel/types";
 
 export default function Main(recipe, onClick) {
   const recipes = JSON.parse(localStorage.getItem("recipesArray"));
 
   return (
     <main className="App-main">
-      <ul>
-        {recipes.map((recipe) => {
-          return (
-            <ItemCard
-              key={recipe.id}
-              name={recipe.cakeName}
-              ingredients={recipe.cakeIngredients}
-            />
-          );
-        })}
-      </ul>
+      <Link to={`/cakes/${recipe.id}`}>
+        <div>
+          {recipes.map((recipe) => {
+            return <ItemCard key={recipe.id} name={recipe.cakeName} />;
+          })}
+        </div>
+      </Link>
     </main>
   );
 }
