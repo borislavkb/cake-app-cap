9704cf09c807708998945ee7569ed837057e800a
Borislav Borislavov <borislavkb@gmail.com>
2021-08-24 08:48:16 +0200

    Remove unnecessary code

--- src/pages/SingleCakePage.js
+++ src/pages/SingleCakePage.js
@@ -2,13 +2,11 @@ import "./SingleCakePage.css";
 import { useParams } from "react-router-dom";
 
 import mockImage from "../images/red.png";
-import { useEffect, useLayoutEffect, useState } from "react";
 
 export default function SingleCakePage() {
   const cakes = JSON.parse(localStorage.getItem("recipesArray"));
 
   const { id } = useParams();
-  const [cake, setCake] = useState(null);
 
   return (
     <main className="SinglePage__content">
