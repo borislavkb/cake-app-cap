8c7ce962bddb36d688b43bed89df00f8ecba09d6
Borislav Borislavov <borislavkb@gmail.com>
2021-08-24 08:46:48 +0200

    Single page renders single cake recipe

--- src/pages/SingleCakePage.js
+++ src/pages/SingleCakePage.js
@@ -13,23 +13,23 @@ export default function SingleCakePage() {
   return (
     <main className="SinglePage__content">
       {cakes.map((cake, index) => {
-        const id = index + 1;
-        return (
-          <div>
-            <h2 className="SinglePage__cake--title" key={cake.id}>
-              {cake.cakeName} {id}
-            </h2>
-            <img
-              src={mockImage}
-              alt={cake.cakeName}
-              className="SinglePage__cake--image"
-            />
-            <h3>Ingredients:</h3>
-            <p>{cake.cakeIngredients}</p>
-            <h3>Recipe: </h3>
-            <p>{cake.cakeRecipe}</p>
-          </div>
-        );
+        if (id == index + 1)
+          return (
+            <div>
+              <h2 className="SinglePage__cake--title" key={cake.id}>
+                {cake.cakeName} {id}
+              </h2>
+              <img
+                src={mockImage}
+                alt={cake.cakeName}
+                className="SinglePage__cake--image"
+              />
+              <h3>Ingredients:</h3>
+              <p>{cake.cakeIngredients}</p>
+              <h3>Recipe: </h3>
+              <p>{cake.cakeRecipe}</p>
+            </div>
+          );
       })}
     </main>
   );
