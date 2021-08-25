2f9ce8bd966badb91da5f4e75638e888f87a4763
Borislav Borislavov <borislavkb@gmail.com>
2021-08-25 16:31:42 +0200

    Add message if there are no items saved/added

--- src/pages/MyCakesList.js
+++ src/pages/MyCakesList.js
@@ -4,6 +4,7 @@ import { RiFilePaper2Line } from "react-icons/ri";
 import ChCakeImg from "../images/chocolate.png";
 import { useEffect, useState } from "react";
 import { MdFavoriteBorder } from "react-icons/md";
+import { GiCakeSlice } from "react-icons/gi";
 
 export default function MyCakesList() {
   const recipesLS = JSON.parse(localStorage.getItem("recipesArray"));
@@ -35,40 +36,48 @@ export default function MyCakesList() {
   }
 
   function renderListOfCakeRecipes() {
-    return listOfCakes?.map((cake, index) => {
-      const id = index + 1;
-      return (
-        <li className="ItemCard" key={cake.id}>
-          <img src={ChCakeImg} alt="description" className="ItemCard__image" />
+    if (listOfCakes.length === 0) {
+      return <p>There are no recipes currently stored. Add your recipe ! </p>;
+    } else {
+      return listOfCakes?.map((cake, index) => {
+        const id = index + 1;
+        return (
+          <li className="ItemCard" key={cake.id}>
+            <img
+              src={ChCakeImg}
+              alt="description"
+              className="ItemCard__image"
+            />
 
-          <h2 className="ItemCard__name">{cake.cakeName}</h2>
-          <div className="ItemCard--btnDiv">
-            <button
-              className="ItemCard__button delete"
-              onClick={() => handleDeleteItemFromList(cake.id)}
-            >
-              X
-            </button>
-            <button className="ItemCard__button--fav">
-              <MdFavoriteBorder
-                size="2rem"
-                color="#d84064"
-                className="ItemCard__toggle"
-                onClick={() => handleToggleFavouriteCake(cake.id)}
-              />
-            </button>
+            <h2 className="ItemCard__name">{cake.cakeName}</h2>
+            <div className="ItemCard--btnDiv">
+              <button
+                className="ItemCard__button delete"
+                onClick={() => handleDeleteItemFromList(cake.id)}
+              >
+                X
+              </button>
+              <button className="ItemCard__button--fav">
+                <MdFavoriteBorder
+                  size="2rem"
+                  color="#d84064"
+                  className="ItemCard__toggle"
+                  onClick={() => handleToggleFavouriteCake(cake.id)}
+                />
+              </button>
 
-            <Link to={`/cakes/${id}`}>
-              <RiFilePaper2Line
-                size="1.6rem"
-                color="#d84064"
-                className="ItemCard__link"
-              />
-            </Link>
-          </div>
-        </li>
-      );
-    });
+              <Link to={`/cakes/${id}`}>
+                <RiFilePaper2Line
+                  size="1.6rem"
+                  color="#d84064"
+                  className="ItemCard__link"
+                />
+              </Link>
+            </div>
+          </li>
+        );
+      });
+    }
   }
 
   return (
