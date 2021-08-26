015694257b9e14a23d99c7d94a2beba122c90895
Borislav Borislavov <borislavkb@gmail.com>
2021-08-26 23:23:20 +0200

    Create env variables to store API credentials

--- src/pages/ListAPI.js
+++ src/pages/ListAPI.js
@@ -1,27 +1,27 @@
 import "./ListAPI.css";
 import { useEffect, useState } from "react";
 import ItemCardAPI from "../components/ItemCardAPI";
+const API_ID = process.env.REACT_APP_API_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default function ListAPI({ object }) {
-  const API_ID = "0cab5025";
-  const API_KEY = "39663efb82058e801b156ea8aee14b54";
-
   const [cakes, setCakes] = useState([]);
 
   useEffect(() => {
-    const url =
-      "https://api.edamam.com/api/recipes/v2?type=public&q=cake&app_id=0cab5025&app_key=39663efb82058e801b156ea8aee14b54";
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=cake&app_id=${API_ID}&app_key=${API_KEY}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setCakes(data.hits);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }, []);
 
   function renderRecipes() {
     return cakes.map((cake, index) => {
       const id = index + 1;
-      console.log(cake);
       return <ItemCardAPI props={cake} />;
     });
   }
