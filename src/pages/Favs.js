a5ff0973a86a471946e3f2daa3d45fdd543589f4
Borislav Borislavov <borislavkb@gmail.com>
2021-08-31 00:58:46 +0200

    Add the merged fix for single page render for testing purposes

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,7 +1,6 @@
 import "./Favs.css";
-import { RiFilePaper2Line } from "react-icons/ri";
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import ItemCard from "../components/ItemCard";
 
 export default function Favs() {
   const allFavouriteCakes = JSON.parse(
@@ -9,22 +8,14 @@ export default function Favs() {
   );
 
   const [favouriteRecipes, setFavouriteRecipes] = useState(allFavouriteCakes);
-  console.log(favouriteRecipes);
+
   function renderListOfFavs() {
     if (favouriteRecipes === null) {
       return <h3>There are no saved favourite items! </h3>;
     } else {
       return favouriteRecipes.map((recipe) => {
         return (
-          <li className="Recipe-api__card" key={recipe.id}>
-            <img src="" alt="" className="Recipe-api__card--image" />
-            <div className="Recipe-api__card--body">
-              <h2 className="Recipe-api__card--title">{recipe.label}</h2>
-              <Link to={`/cakes/${recipe.id}`}>
-                <RiFilePaper2Line className="Icon" />
-              </Link>
-            </div>
-          </li>
+          <ItemCard object={recipe} paramsID={recipe.id} onDelete onToggleFav />
         );
       });
     }
--- src/pages/SingleCakePage.js
+++ src/pages/SingleCakePage.js
@@ -1,19 +1,31 @@
 import "./SingleCakePage.css";
+import { useParams } from "react-router-dom";
+import chocolateCake from "../images/chocolate.png";
 
 export default function SingleCakePage() {
+  const { id } = useParams();
+
+  const recipesArray = JSON.parse(localStorage.getItem("recipesArray")) || [];
+
   function renderSingleCake() {
-    return (
-      <main className="SinglePage__content">
-        <div>
-          <h2 className="SinglePage__cake--title">Item title</h2>
-          <img src="" alt="" className="SinglePage__cake--image" />
-          <h3>Ingredients:</h3>
-          <p>Item 1</p>
-          <h3>Recipe: </h3>
-          <p>Recipe 1</p>
-        </div>
-      </main>
-    );
+    return recipesArray
+      .filter((cake) => cake.id === id)
+      .map((cake) => (
+        <main className="SinglePage__content">
+          <div>
+            <h2 className="SinglePage__cake--title">{cake.cakeName}</h2>
+            <img
+              src={chocolateCake}
+              alt=""
+              className="SinglePage__cake--image"
+            />
+            <h3>Ingredients:</h3>
+            <p>{cake.cakeIngredients}</p>
+            <h3>Recipe: </h3>
+            <p>{cake.cakeRecipe}</p>
+          </div>
+        </main>
+      ));
   }
 
   return renderSingleCake();
