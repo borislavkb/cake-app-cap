f90d75e4e570797da53c5b9f7348473074f7f3be
Borislav Borislavov <borislavkb@gmail.com>
2021-08-31 14:25:00 +0200

    Change icons on CardItem

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,7 +2,7 @@ import "./ItemCard.css";
 import ChCakeImg from "../images/chocolate.png";
 import { RiFilePaper2Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { MdFavoriteBorder } from "react-icons/md";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
 
 export default function ItemCard({ object, paramsId, onDelete, onToggleFav }) {
   function handleDeleteItemFromList() {
@@ -26,10 +26,17 @@ export default function ItemCard({ object, paramsId, onDelete, onToggleFav }) {
           X
         </button>
         <button className="ItemCard__button--fav">
-          <MdFavoriteBorder
-            className="Icon"
-            onClick={() => handleToggleFavouriteCake(object.id)}
-          />
+          {object.isFav ? (
+            <IoHeartOutline
+              className="Icon"
+              onClick={() => handleToggleFavouriteCake(object.id)}
+            />
+          ) : (
+            <IoHeart
+              className="Icon"
+              onClick={() => handleToggleFavouriteCake(object.id)}
+            />
+          )}
         </button>
 
         <Link to={`/cakes/${paramsId}`}>
