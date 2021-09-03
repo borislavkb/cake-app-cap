5e75865442201dee0c35462e950abd68295459cc
Borislav Borislavov <borislavkb@gmail.com>
2021-09-04 00:05:50 +0200

    Add space lining 0.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,19 @@ export default function Header() {
     <header className="Header">
       <Switch>
         <Route path="/API">
-          <h1>Random recipes from the Internet !</h1>
+          <h1 className="Header__title">Random recipes from the Internet !</h1>
         </Route>
         <Route path="/cakes/:id">
-          <h1>Single MyCakes</h1>
+          <h1 className="Header__title">Single MyCakes</h1>
         </Route>
         <Route path="/favs">
-          <h1>Favourites</h1>
+          <h1 className="Header__title">Favourites</h1>
         </Route>
         <Route path="/add">
-          <h1>Insert a new cake</h1>
+          <h1 className="Header__title">Insert a new cake</h1>
         </Route>
         <Route exact path="/">
-          <h1> Catalogue</h1>
+          <h1 className="Header__title"> Catalogue</h1>
         </Route>
       </Switch>
     </header>
