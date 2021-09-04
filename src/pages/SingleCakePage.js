34105f21a53c52067992fcf560ec4831274995e0
Borislav Borislavov <borislavkb@gmail.com>
2021-09-04 17:13:47 +0200

    Add a button and logic to use useHistory

--- src/pages/SingleCakePage.js
+++ src/pages/SingleCakePage.js
@@ -1,9 +1,15 @@
 import "./SingleCakePage.css";
 import { useParams } from "react-router-dom";
 import defaultImage from "../images/chocolate.png";
+import { useHistory } from "react-router-dom";
 
 export default function SingleCakePage() {
   const { id } = useParams();
+
+  let history = useHistory();
+  function returnToHome() {
+    return history.push("/");
+  }
 
   const recipesArray = JSON.parse(localStorage.getItem("recipesArray")) || [];
 
@@ -23,6 +29,13 @@ export default function SingleCakePage() {
             <p className="SinglePage__cake--text">{cake.cakeIngredients}</p>
             <h3 className="SinglePage__cake--title">Recipe: </h3>
             <p className="SinglePage__cake--text">{cake.cakeRecipe}</p>
+            <button
+              type="button"
+              className="SinglePage_cake--button"
+              onClick={returnToHome}
+            >
+              Return home
+            </button>
           </div>
         </main>
       ));
